e29f0c60ecb58ed343d1865e54e71e54e3122eb5
andrewwilliamp <84424039+andrewwilliamp@users.noreply.github.com>
2024-04-18 15:50:35 -0600

    Update streamlit_app.py
    test pandas df conversion

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ from snowflake.snowpark.functions import col
 # Display nutrition info
 import requests
 
+import pandas as pd
+
 
 
 
@@ -22,8 +24,14 @@ cnx = st.connection("snowflake")
 #session = get_active_session()
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert to pandas df
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 ingredients_list = st.multiselect(
